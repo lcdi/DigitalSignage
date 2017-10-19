9a9e5a49cb1c560aef3cb5c5bb8e725874b3cb72
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2017-10-19 16:25:32 -0400

    Changes to Tables

---
+++ src/tables/remove.php
@@ -0,0 +1,21 @@
+<?php
+    require_once 'login.php';
+    require_once '../php/sanitize.php';
+    $conn = new mysqli($hn,$un,$pw,$db);
+    $response = "";
+    if($conn->connect_error) die($conn->connect_error);
+    
+    if (isset($_POST['zip']))
+    {
+        $zip = santitizeMySQL($conn, $_POST['zip']);
+        $response += $zip;
+        $query = "DELETE FROM weather WHERE zip='$zip'";
+        $result = $conn->query($query);
+        
+    }else
+    {
+        die("Query Fail\n" . $_POST['zip']);
+    }
+    $conn->close();
+    echo("Successful Query!\n");
+?>
