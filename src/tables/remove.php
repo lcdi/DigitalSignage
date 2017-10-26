43d59ade86383461523b865dc1bc7df5b6a88939
Alexander West <awest@Research.lcdi>
2017-10-26 18:07:23 -0400

    Fixed remove and edit bugs

--- src/tables/remove.php
+++ src/tables/remove.php
@@ -5,11 +5,12 @@
     $response = "";
     if($conn->connect_error) die($conn->connect_error);
     
-    if (isset($_POST['zip']))
+    if (isset($_POST['zip']) && isset($_POST['country']))
     {
         $zip = santitizeMySQL($conn, $_POST['zip']);
+        $country = santitizeMySQL($conn, $_POST['country']);
         $response += $zip;
-        $query = "DELETE FROM weather WHERE zip='$zip'";
+        $query = "DELETE FROM weather WHERE zip='$zip AND WHERE country='$country'";
         $result = $conn->query($query);
         
     }else
