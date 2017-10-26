8e713c7ab5f7dceae2b62084d38ce48ffc9a0820
Alexander West <awest@Research.lcdi>
2017-10-26 19:29:01 -0400

    Dynamic changes to tables

--- src/js/bootmysql.js
+++ src/js/bootmysql.js
@@ -1,8 +1,82 @@
 $(document).ready(()=>{
     var $table = $('.display');
     var $modal = $('#message');
+
+    // Table info for bootstrap table generation and SQL query
+    var tableArray = [
+        {
+            "table" : "weather",
+            "numCols" : 2, 
+            "col0":{ 
+                "field" : "zip",
+                "title" : "Zip"
+            },
+            "col1":{
+                "field" : "country",
+                "title" : "Country"
+            }
+        },{
+            "table" : "project_hours",
+            "numCols" : 3,
+            "col0" : {
+                "field" : "name",
+                "title" : "Project Name"
+            },
+            "col1" : {
+                "field" : "people",
+                "title" : "People"
+            },
+            "col2" : {
+                "field" : "hours",
+                "title" : "Hours"
+            }
+        }
+    ];
+
+    // To be pulled from the html page?
+    // Possibly on a click
+    var tableNumIndex = 0;
+
+    var tableArray = [weather_info, project_hours_info];
+
+    // Grab the specific table that you are going to look at
+    var table = tableArray[tableNumIndex];
+
+    // Table data is the columns for bootstrap
+    var tableData = [];
+    for(var i = 0; i < table["numCols"]; i++)
+    {
+        console.log(table["col"+i]);
+        // Get the column information and push 
+        // the JSON object to the array
+        var col = table["col"+i];
+        tableData.push(
+            {
+                field: col["field"],
+                title: col["title"]
+            }
+        );
+    }
+
+    // Push the option buttons to the array
+    tableData.push({
+        title: 'options',
+        field: 'Options',
+        align: 'center',
+        events: operateEvents,
+        formatter: (response)=>
+        {
+            return [
+                '<button class="btn glyphicon glyphicon-trash remove"></button> ',
+                '<button class="btn glyphicon glyphicon-pencil edit"></button>'
+            ].join('');
+        }
+    });
+
+    console.log(tableData);
+
     var operateEvents = {
-        'click .remove':function(e, value, row, index) {
+        'click .remove':(e, value, row, index)=> {
             $.ajax({
                 type: 'POST',
                 url: '../tables/remove.php',
@@ -19,14 +93,14 @@ $(document).ready(()=>{
                 }
             });
         },
-        'click .edit': function(e, value, row, index) {
+        'click .edit': (e, value, row, index)=> {
             $("#message .modal-body").html('<form id="edit_form" action="#"><div class="form-group"><label for="zip">Zip Code</label><input type="text" class="form-control" id="zip" name="zip" placeholder="'+row.zip+'"></div><label for="country">Country</label><input type="text" class="form-control" id="country" name="country" placeholder="'+row.country+'"></div><button type="submit" class="btn btn-primary">Submit</button></form>');
             $modal.on('show.bs.modal', ()=>{}).modal("show");
             $("#edit_form").submit((event)=>
             {
-                const zip = document.getElementById("zip").value;
-                const country = document.getElementById("country").value;
-
+                const zip = $('#zip').val();
+                const country = $("#country").val();
+                console.log(zip + " " + country);
                 var data = {"zip":zip, "country":country, "oZip":row.zip, "oCountry":row.country};
                 if(zip === "")
                 {
@@ -36,7 +110,6 @@ $(document).ready(()=>{
                 {
                     data.country = row.country;
                 }
-                event.preventDefault();
                 $.ajax({
                     type: 'POST',
                     url: '../tables/edit.php',
@@ -44,7 +117,7 @@ $(document).ready(()=>{
                     success:(response)=>
                     {
                         location.reload();
-                        window.onbeforeunload = function(e) {
+                        window.onbeforeunload = (e)=> {
                             // Turning off the event
                             e.preventDefault();
                         }
@@ -58,36 +131,27 @@ $(document).ready(()=>{
                 })
             });
         }
-    };  
-    
+    };
+
+    //Use this to determine the table used
+    var data = {'table':table["table"]};
+    $.ajax(
+    {
+        type: 'POST',
+        url: 'loadtable.php',
+        data: data,
+        success: (response)=>
+        { 
+            //console.log(response);
             $table.bootstrapTable({
-                url: 'loadtable.php', 
+                contentType:'application/json',
+                data: JSON.parse(response),
                 search: true,
                 pagination: true,
                 buttonsClass: 'primary',
                 showFooter: true,
-                minimumCountColumns: 2,
-                columns: [{
-                    field: 'zip',
-                    title: 'zip',
-                    sortable: true,
-                },{
-                    field: 'country',
-                    title: 'country',
-                    sortable: true,
-                },{
-                    title: 'options',
-                    field: 'options',
-                    align: 'center',
-                    events: operateEvents,
-                    formatter: ()=>
-                    {
-                        return [
-                            '<button class="btn glyphicon glyphicon-trash remove"></button> ',
-                            '<button class="btn glyphicon glyphicon-pencil edit"></button>'
-                        ].join('');
-                    }
-                }],
+                minimumCountColumns: 3,
+                columns: tableData,
             });
-
-        });
+        }});
+})
