1641721d47b5186cedf7d9330fe23ae584ff4a6c
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2017-11-09 14:34:21 -0500

    renaming bootmyql => tables

--- src/html/bootmysql.php
+++ src/html/bootmysql.php
@@ -75,6 +75,6 @@ if(!isset($_SESSION['user']))
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script src="../js/bootstrap-table.js"></script>
-		<script src="../js/bootmysql.js"></script>
+		<script src="../js/tables.js"></script>
 </body>
 </html>
